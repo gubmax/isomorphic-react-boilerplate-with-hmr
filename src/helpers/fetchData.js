40df07168c711bb3633509c58891643d15db72d7
gubmax <gubyshev@gmail.com>
2020-02-15 17:44:04 +0300

    Add api prefix for routes

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch'
 
-const { APP_PROTOCOL, APP_HOST, APP_SERVER_PORT } = process.env
+const {
+  APP_PROTOCOL, APP_HOST, APP_SERVER_PORT, APP_API_PREFIX,
+} = process.env
 
 const fetchData = async (url, options) => (
-  fetch(`${APP_PROTOCOL}://${APP_HOST}:${APP_SERVER_PORT}${url}`, options)
+  fetch(`${APP_PROTOCOL}://${APP_HOST}:${APP_SERVER_PORT}${APP_API_PREFIX}${url}`, options)
     .then((res) => res.json())
 )
 
--- src/server/router.js
+++ src/server/router.js
@@ -3,10 +3,12 @@ import Router from 'koa-router'
 import { findUsers } from './controllers/Users'
 import serverRenderer from './serverRenderer'
 
+const { APP_API_PREFIX } = process.env
+
 const router = new Router()
 
 router
-  .post('/users', findUsers)
+  .post(`${APP_API_PREFIX}/users`, findUsers)
   .get('/*', serverRenderer)
 
 export default router
